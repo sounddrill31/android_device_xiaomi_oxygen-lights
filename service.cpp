6617253820f85140ec88ce2ce70d658766b93d14
SoundDrill31 <84176052+sounddrill31@users.noreply.github.com>
2024-03-21 21:20:06 +0530

    Lights: Rename service to android.hardware.lights-service.xiaomi

--- service.cpp
+++ service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "android.hardware.light@2.0-service.xiaomi_oxygen"
+#define LOG_TAG "android.hardware.lights-service.xiaomi"
 
 #include <hidl/HidlTransportSupport.h>
 
